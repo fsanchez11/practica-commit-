4ba3acfd447c60b3d37fa48a910a071bc2d7d812
Felipe <ljesus530@gmail.com>
2018-08-29 19:08:18 -0500

    evento clic al enlace bienvenido

--- resources/views/indexgit.blade.php
+++ resources/views/indexgit.blade.php
@@ -19,7 +19,7 @@
   <!-- Links -->
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="#">Link 1</a>
+      <a class="nav-link" href="#irbienv">Bienvenido</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Link 2</a>
@@ -54,6 +54,37 @@
 <button type="button" class="btn btn-default">Default</button>
 <button type="button" class="btn btn-light">Light</button>
 <button type="button" class="btn btn-link">Link</button>
+
+<hr><br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <hr>
+
+  <hr><br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <hr>
+
+  <hr><br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <hr>
+
+  <h3 id="irbienv">Bienvenido</h3>
+
+
 
 
 </body>
